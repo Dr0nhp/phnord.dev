33a1bef2668c680f96fb3eb6494b2a5ab8e1741e
Daniel <13246131+Dr0nhp@users.noreply.github.com>
2025-11-09 23:25:54 +0100

    Initial Elements

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,29 @@
 <template>
-  <div class="app">
-    <h1>X</h1>
-    <p>Deployment Test</p>
+  <div id="app">
+    <TheHeader />
+
+    <main class="page-content-wrapper">
+      <RouterView />
+    </main>
+
+    <TheFooter />
   </div>
 </template>
+
+<script setup>
+import TheHeader from './components/TheHeader.vue'
+import TheFooter from './components/TheFooter.vue'
+</script>
+
+<style scoped>
+
+#app {
+  min-height: 99vh;
+  display: flex;
+  flex-direction: column;
+}
+
+.page-content-wrapper {
+  flex-grow: 1;
+}
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="homepage">
+
+    <div class="card">
+      <h2>Card</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+    </div>
+
+  </div>
+</template>
