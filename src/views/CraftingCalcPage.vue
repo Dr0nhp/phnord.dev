49a1c1e59c3efa9452daa5efa36fc18225bea9dc
Daniel <13246131+Dr0nhp@users.noreply.github.com>
2026-01-05 22:44:16 +0100

    added ffxivCraftingMatCalc

---
+++ src/views/CraftingCalcPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="crafting-calc-wrapper">
+    <iframe
+      src="/ffxivCraftingMatCalc/index.html"
+      title="FFXIV Crafting Calculator"
+      class="crafting-calc-iframe"
+    ></iframe>
+  </div>
+</template>
+
+<style scoped>
+.crafting-calc-wrapper {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.crafting-calc-iframe {
+  width: 100%;
+  height: 100%;
+  border: none;
+}
+</style>
