a9f0353aa5d3fa9fbc226de08a0cfceb3e206220
Daniel <13246131+Dr0nhp@users.noreply.github.com>
2026-01-06 12:34:00 +0100

    fixes

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <template>
-  <TheHeader />
+  <TheHeader v-if="!isFullPageRoute" />
 
-  <main class="page-content-wrapper">
+  <main class="page-content-wrapper" :class="{ 'full-page': isFullPageRoute }">
     <RouterView />
   </main>
 
-  <TheFooter />
+  <TheFooter v-if="!isFullPageRoute" />
 </template>
 
 <script setup>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
 import TheHeader from './components/TheHeader.vue'
 import TheFooter from './components/TheFooter.vue'
+
+const route = useRoute()
+
+// Hide header/footer for full-page routes like the crafting calculator
+const isFullPageRoute = computed(() => {
+  return route.path === '/crafting-calc'
+})
 </script>
+
+<style scoped>
+.page-content-wrapper.full-page {
+  padding: 0;
+  margin: 0;
+}
+</style>
